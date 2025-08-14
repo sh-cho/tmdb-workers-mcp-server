619aed765dfac11d3841c4707a05907c74de0675
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2025-08-14 22:39:19 +0900

    tmdb api
    Signed-off-by: Seonghyeon Cho <seonghyeoncho96@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,82 @@
 import { McpAgent } from "agents/mcp";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { z } from "zod";
+import { MovieSearchOptions, TMDB } from "@tdanks2000/tmdb-wrapper";
+import { fmt } from "./utils/format";
 
-// Define our MCP agent with tools
-export class MyMCP extends McpAgent {
+export class TmdbMCP extends McpAgent {
 	server = new McpServer({
-		name: "Authless Calculator",
-		version: "1.0.0",
+		name: "TMDB Mcp Server",
+		version: "0.1.0",
 	});
 
 	async init() {
-		// Simple addition tool
-		this.server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-			content: [{ type: "text", text: String(a + b) }],
-		}));
-
-		// Calculator tool with multiple operations
 		this.server.tool(
-			"calculate",
+			"search_movies",
+			fmt.trim(`
+				Search for movies by title.
+
+				If no query is provided, returns popular movies.
+			`),
 			{
-				operation: z.enum(["add", "subtract", "multiply", "divide"]),
-				a: z.number(),
-				b: z.number(),
+				query: z.string().optional(),
 			},
-			async ({ operation, a, b }) => {
-				let result: number;
-				switch (operation) {
-					case "add":
-						result = a + b;
-						break;
-					case "subtract":
-						result = a - b;
-						break;
-					case "multiply":
-						result = a * b;
-						break;
-					case "divide":
-						if (b === 0)
-							return {
-								content: [
-									{
-										type: "text",
-										text: "Error: Cannot divide by zero",
-									},
-								],
-							};
-						result = a / b;
-						break;
+			async ({ query }) => {
+				const token = this.props.tmdbAccessToken as string;
+				const tmdb = new TMDB(token);
+
+				if (!query) {
+					const popularMovies = await tmdb.movies.popular({
+						language: 'ko-KR'
+					});
+					return {
+						content: popularMovies.results.map((movie) => ({
+							type: "text",
+							text: movie.title,
+						})),
+					};
 				}
-				return { content: [{ type: "text", text: String(result) }] };
+
+				const searchOption: MovieSearchOptions = {
+					query,
+					include_adult: false,
+					language: 'ko-KR',
+				};
+				const searchResults = await tmdb.search.movies(searchOption);
+				return {
+					content: searchResults.results.map((movie) => ({
+						type: "text",
+						text: movie.title,
+					})),
+				};
+			}
+		);
+
+		this.server.tool(
+			"get_trending_movies",
+			fmt.trim(`
+				Get a list of trending movies.
+
+				MUST provide the time window for trending movies.
+				time window can be either "day" or "week".
+			`),
+			{
+				timeWindow: z.enum(["day", "week"]).describe("Time window for trending movies"),
 			},
+			async ({ timeWindow }) => {
+				const token = this.props.tmdbAccessToken as string;
+				const tmdb = new TMDB(token);
+
+				const trendingMovies = await tmdb.trending.trending("movie", timeWindow, {
+					language: 'ko-KR',
+				});
+				return {
+					content: trendingMovies.results.map((movie) => ({
+						type: "text",
+						text: movie.title,
+					})),
+				};
+			}
 		);
 	}
 }
@@ -58,12 +85,18 @@ export default {
 	fetch(request: Request, env: Env, ctx: ExecutionContext) {
 		const url = new URL(request.url);
 
+		const tmdbAccessToken = request.headers.get("X-TMDB-ACCESS-TOKEN");
+		if (!tmdbAccessToken) {
+			return new Response("Unauthorized", { status: 401 });
+		}
+		ctx.props.tmdbAccessToken = tmdbAccessToken;
+
 		if (url.pathname === "/sse" || url.pathname === "/sse/message") {
-			return MyMCP.serveSSE("/sse").fetch(request, env, ctx);
+			return TmdbMCP.serveSSE("/sse").fetch(request, env, ctx);
 		}
 
 		if (url.pathname === "/mcp") {
-			return MyMCP.serve("/mcp").fetch(request, env, ctx);
+			return TmdbMCP.serve("/mcp").fetch(request, env, ctx);
 		}
 
 		return new Response("Not found", { status: 404 });
